2b352884948a7b2b2452cb242bdce4dccdc92755
infonaren <infonaren55@gmail.com>
2024-03-25 17:53:28 +0530

    https://github.com/baggy-naren/UiPath-Coded-Workflow.git

---
+++ CodeWorkflow.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Test_Unattended_Bot.ObjectRepository;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace Test_Unattended_Bot
+{
+    public class CodeWorkflow : CodedWorkflow
+    {
+        [Workflow]
+        public void Execute()
+        {
+            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
+            // e.g. system.GetAsset(...)
+
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+            var WelcomeMessage = "This is the C# Coded Workflow in UiPath Studio";
+            Console.WriteLine(WelcomeMessage);
+        }
+    }
+}
